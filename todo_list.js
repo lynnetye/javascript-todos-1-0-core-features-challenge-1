cf2c08662ff587248a26e69250ce003850bebf91
Lynne Tye & Lynne Tye <pair+lynnetye.lynnetye@devbootcamp.com>
2015-04-11 21:14:06 -0700

    first release

--- todo_list.js
+++ todo_list.js
@@ -1,14 +1,24 @@
 var createTodoList = function() {
-  var todoList = {};
+  var todoList = {
+    items: [],
+    add: function(item) {
+      this.items.push(item)
+    },
+    list: function() {
+      return this.items;
+    },
+    indexOf: function(item) {
+      return this.items.indexOf(item);
+    },
+    remove: function(indexOfItem) {
+      this.items.splice(indexOfItem, 1);
+    }
+  };
 
   // your code here
 
   return todoList;
 };
-
-
-
-
 
 
 // Driver code
@@ -31,20 +41,20 @@ groceryList.add('bread');
 groceryList.add('cheese');
 groceryList.add('milk');
 groceryList.list(); //-> [
-// {description: 'bread', completed: false}, 
-// {description: 'cheese', completed: false}, 
-// {description: 'milk', completed: false}, 
+// {description: 'bread', completed: false},
+// {description: 'cheese', completed: false},
+// {description: 'milk', completed: false},
 // ];
 groceryList.indexOf('cheese'); //-> 1
 groceryList.get(1); //-> {description: 'cheese', completed: false}
 groceryList.complete(1);
 groceryList.list(); //-> [
-// {description: 'bread', completed: false}, 
-// {description: 'cheese', completed: true}, 
-// {description: 'milk', completed: false}, 
+// {description: 'bread', completed: false},
+// {description: 'cheese', completed: true},
+// {description: 'milk', completed: false},
 // ];
 groceryList.remove(1);
 groceryList.list(); //-> [
-// {description: 'bread', completed: false}, 
-// {description: 'milk', completed: false}, 
+// {description: 'bread', completed: false},
+// {description: 'milk', completed: false},
 // ];
